15fccd9908e119a9d7b34372c99657a48a05a53a
Bruce Esplin <bwesplin@gmail.com>
2018-06-18 11:41:04 -0400

    Started Calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -8,8 +8,10 @@ import { LoginComponent } from './views/user/login/login.component';
 import { RegisterComponent } from './views/user/register/register.component';
 import { RegisterNurseComponent } from './views/user/register-nurse/register-nurse.component';
 
-
+import { CalendarModule } from "angular-calendar";
 import { routing } from './app.routing';
+import {DataTableModule} from "angular-6-datatable"
+import { CalendarComponent } from './views/user/calendar/calendar.component';
 
 
 
@@ -20,12 +22,17 @@ import { routing } from './app.routing';
     LoginComponent,
     RegisterComponent,
     RegisterNurseComponent,
+    CalendarComponent,
     
   ],
   imports: [
     ReactiveFormsModule,
+    FormsModule,
     BrowserModule,
-    routing
+    routing,
+    CalendarModule,
+    DataTableModule
+
     
   ],
   providers: [],
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,11 +3,13 @@ import {  Routes, RouterModule  } from '@angular/router';
 import { LoginComponent} from './views/user/login/login.component';
 import { RegisterComponent }  from './views/user/register/register.component';
 import { RegisterNurseComponent } from './views/user/register-nurse/register-nurse.component';
+import { CalendarComponent } from './views/user/calendar/calendar.component';
 
 const appRoutes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register_patient', component: RegisterComponent },
-    { path: 'register_nurse', component: RegisterNurseComponent}
+    { path: 'register_nurse', component: RegisterNurseComponent},
+    { path: 'calendar', component: CalendarComponent }
 ];
 
 
